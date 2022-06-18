15e26ec0e1a8e3a0b8758e662daa571fb152d883
Jason Swartz <dev@bks2.com>
2022-06-17 21:51:25 -0700

    removed unused items

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -31,19 +31,6 @@ class Colors {
 		})
 		return StringUtils.toTextBlock(coloredLines)
 	}
-
-	// Remove colors from a string
-	static strip = (s: string): string => s.replace(Colors.ansiRegex(), '')
-
-	// From https://github.com/chalk/ansi-regex/blob/02fa893d619d3da85411acc8fd4e2eea0e95a9d9/index.js, thx!
-	static ansiRegex(): RegExp {
-		const pattern = [
-			'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
-			'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
-		].join('|')
-
-		return new RegExp(pattern, 'g')
-	}
 }
 
 enum Xterm256 {
