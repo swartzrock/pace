d01c052a0870b13f69e8a563a801084cf498d8dd
Jason Swartz <dev@bks2.com>
2022-07-04 16:09:16 -0700

    new 2d array functions

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -12,6 +12,19 @@ class Utils {
 		}
 		return a[Math.floor(Math.random() * a.length)]
 	}
+
+	static grouped<A>(a: Array<A>, groupSize: number): Array<Array<A>> {
+		const result = new Array<Array<A>>()
+		for (let i = 0; i < a.length; i += groupSize) {
+			result.push(a.slice(i, i + groupSize))
+		}
+
+		return result
+	}
+
+	static transpose<A>(matrix: Array<Array<A>>): Array<Array<A>> {
+		return matrix[0].map((col, i) => matrix.map((row) => row[i]))
+	}
 }
 
 export { Utils }
