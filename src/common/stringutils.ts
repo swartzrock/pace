a44d09ea20ef9ab956c5a295a4e25737361f7f30
Jason Swartz <dev@bks2.com>
2022-07-04 16:09:50 -0700

    new string utils functions

--- src/common/stringutils.ts
+++ src/common/stringutils.ts
@@ -14,32 +14,46 @@ class StringUtils {
 		return s.slice(0, i) + c + s.slice(i + 1, s.length)
 	}
 
-	// static centerTextBlockInTextBlock(colorableForeground: string, background: string): string {
-	// 	const fgLines = StringUtils.toLines(colorableForeground)
-	// 	const fgWidth = Colors.strip(fgLines[0]).length
-	// 	const fgHeight = fgLines.length
-	// 	const bgLines = StringUtils.toLines(background)
-	// 	const bgWidth = bgLines[0].length
-	// 	const bgHeight = bgLines.length
-	//
-	// 	if (fgWidth > bgWidth || fgHeight > bgHeight) {
-	// 		console.log("centerTextBlockInTextBlock, foreground can't be bigger than background")
-	// 		return background
-	// 	}
-	//
-	// 	const firstRow = Utils.halfInt(bgHeight) - Utils.halfInt(fgHeight)
-	// 	const firstCol = Utils.halfInt(bgWidth) - Utils.halfInt(fgWidth)
-	//
-	// 	for (let fgRow = 0; fgRow < fgHeight; fgRow++) {
-	// 		const bgRow = fgRow + firstRow
-	// 		const leftBackground = bgLines[bgRow].slice(0, firstCol)
-	// 		const centerForeground = fgLines[fgRow]
-	// 		const rightBackground = bgLines[bgRow].slice(firstCol + fgWidth, bgWidth)
-	// 		bgLines[bgRow] = leftBackground + centerForeground + rightBackground
-	// 	}
-	//
-	// 	return StringUtils.toTextBlock(bgLines)
-	// }
+	/**
+	 * Return the real width of a string w/o color codes
+	 * from [ansi-regex](https://github.com/chalk/ansi-regex)
+	 * @param s the string to measure
+	 */
+	static realTextLength(s: string): number {
+		const pattern = [
+			'[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+			'(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-nq-uy=><~]))',
+		].join('|')
+		const regex = new RegExp(pattern, 'g')
+
+		return s.replaceAll(regex, '').length
+	}
+
+	/**
+	 * Render a matrix of text strings so the columns are aligned,
+	 * with rows separated by newlines
+	 * @param matrix text 2d array
+	 * @param horizPaddingSpaces number of spaces between columns
+	 */
+	static renderAlignedColumns(matrix: string[][], horizPaddingSpaces: number): string {
+		const columns = Math.min(...matrix.map((a) => a.length))
+
+		const maxWidths = new Array<number>(columns)
+		for (let col = 0; col < columns; col++) {
+			maxWidths[col] = Math.max(...matrix.map((row) => this.realTextLength(row[col])))
+		}
+
+		let buf = ''
+		for (const row of matrix) {
+			for (let col = 0; col < columns; col++) {
+				const spacing = maxWidths[col] - this.realTextLength(row[col]) + horizPaddingSpaces
+				buf = buf + row[col] + this.fillString(' ', spacing)
+			}
+			buf = buf + '\n'
+		}
+
+		return buf
+	}
 }
 
 export { StringUtils }
