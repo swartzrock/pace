81175a05a697578c79506bdd06b3fc2b1eb5c6eb
Jason Swartz <dev@bks2.com>
2022-07-25 22:20:13 -0700

    Slant now left-aligns for a more stable animation

--- src/common/textblocks.ts
+++ src/common/textblocks.ts
@@ -1,4 +1,5 @@
 import { StringUtils } from './stringutils'
+import { Rectangle } from './Rectangle'
 
 /**
  * A collection of utilities for working with multiline strings
@@ -9,6 +10,8 @@ class TextBlocks {
 		const widenedLines = lines.map((line) => [...line].map((c) => c + c).join(''))
 		return StringUtils.toTextBlock(widenedLines)
 	}
+
+	static maxRowWidth = (s: string) => Math.max(...StringUtils.toLines(s).map((s1) => s1.length))
 
 	static setPadding(input: string, horiz: number, vert: number, padChar: string): string {
 		const trimmed = this.trimToContent(input)
@@ -32,6 +35,27 @@ class TextBlocks {
 
 		return StringUtils.toTextBlock(vertPadding.concat(horizPaddedLines, vertPadding))
 	}
+
+	static setAllPadding(input: string, padding: Rectangle, padChar: string): string {
+		const trimmed = this.trimToContent(input)
+		return this.addAllPadding(trimmed, padding, padChar)
+	}
+
+	static addAllPadding(input: string, padding: Rectangle, padChar: string): string {
+		const lines = StringUtils.toLines(input)
+
+		const leftFill = StringUtils.fillString(padChar, padding.left)
+		const rightFill = StringUtils.fillString(padChar, padding.right)
+		const horizPaddedLines = lines.map((line) => leftFill + line + rightFill)
+		const paddedFirstLineWidth = horizPaddedLines[0].length
+
+		const vertPaddingFill = StringUtils.fillString(padChar, paddedFirstLineWidth)
+		const topPaddedLines = new Array<string>(padding.top).fill(vertPaddingFill)
+		const bottomPaddedLines = new Array<string>(padding.bottom).fill(vertPaddingFill)
+
+		return StringUtils.toTextBlock(topPaddedLines.concat(horizPaddedLines, bottomPaddedLines))
+	}
+
 	static toString2dArray(s: string): string[][] {
 		return StringUtils.toLines(s).map((line) => line.split(''))
 	}
--- src/renderers/slant.ts
+++ src/renderers/slant.ts
@@ -3,20 +3,29 @@ import { StringMatrix } from '../common/stringmatrix'
 import { TimerDetails, TimerRenderer } from './timer-renderer'
 import { FigletFonts, Fonts } from '../common/fonts'
 import { TextBlocks } from '../common/textblocks'
-import { StringUtils } from '../common/stringutils'
+import { Rectangle } from '../common/Rectangle'
 
 class Slant implements TimerRenderer {
+	private static TIME_REMAINING_FONT = FigletFonts.SLANT_RELIEF
+	private static MAX_SLANT_WIDTH = 87 // width of '55:55' in Slant font
+	private static VERTICAL_PADDING = 10
+
 	/**
 	 * Entrypoint - renders this pie chart to a StringMatrix for later printing to the console
 	 * @param details information about the current timer in-progress
 	 */
 	render(details: TimerDetails): StringMatrix {
-		let timeRemaining = Slant.renderTimeRemainingFiglet(details)
-		const timeRemainingLines = StringUtils.toLines(timeRemaining)
-		const horizMargin = Math.floor((process.stdout.columns - timeRemainingLines[0].length) / 2)
-		const vertMargin = 10
-		timeRemaining = TextBlocks.addPadding(timeRemaining, horizMargin, vertMargin, ' ')
-		const timeRemainingMatrix = StringMatrix.fromMultilineMonochromeString(timeRemaining)
+		const timeRemaining = Slant.renderTimeRemainingFiglet(details)
+		const maxRowWidth = TextBlocks.maxRowWidth(timeRemaining)
+		const terminalWidth = process.stdout.columns
+
+		const leftPadding = Math.floor((terminalWidth - Slant.MAX_SLANT_WIDTH) / 2)
+		const rightPadding = terminalWidth - leftPadding - maxRowWidth
+		const paddings = new Rectangle(leftPadding, Slant.VERTICAL_PADDING, rightPadding, Slant.VERTICAL_PADDING)
+
+		const paddedTimeRemaining = TextBlocks.addAllPadding(timeRemaining, paddings, ' ')
+
+		const timeRemainingMatrix = StringMatrix.fromMultilineMonochromeString(paddedTimeRemaining)
 		timeRemainingMatrix.replaceAll(' ', '_')
 		timeRemainingMatrix.replaceAll('_', Colors.foregroundColor('_', Xterm256.CYAN_1))
 
@@ -25,8 +34,7 @@ class Slant implements TimerRenderer {
 
 	private static renderTimeRemainingFiglet(details: TimerDetails): string {
 		const timeRemaining = details.timeRemainingText()
-		const timeRemainingFont = FigletFonts.SLANT_RELIEF
-		const timeRemainingFiglet = Fonts.render(timeRemainingFont, timeRemaining)
+		const timeRemainingFiglet = Fonts.render(this.TIME_REMAINING_FONT, timeRemaining)
 		return TextBlocks.setPadding(timeRemainingFiglet, 1, 1, ' ')
 	}
 }
