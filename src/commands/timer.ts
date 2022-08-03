8ef1b1492e3f02aab96590affeedbdc9cd807f74
Jason Swartz <dev@bks2.com>
2022-08-02 21:48:46 -0700

    First phase of pausing the timer

--- src/commands/timer.ts
+++ src/commands/timer.ts
@@ -4,6 +4,15 @@ import { Utils } from '../common/utils'
 import { StringMatrix } from '../common/stringmatrix'
 import onExit from 'signal-exit'
 import { AnsiCursor } from '../common/ansicursor'
+import { Xterm256 } from '../common/colors'
+import { Rectangle } from '../common/Rectangle'
+import * as readline from 'readline'
+
+enum TimerState {
+	RUN,
+	PAUSE_STARTED_SHOW_MSG,
+	PAUSED,
+}
 
 export default class Timer extends Command {
 	static description = 'Displays a progress timer'
@@ -38,18 +47,51 @@ export default class Timer extends Command {
 	iterations = 0
 	totalIterations = 0
 
-	private executeRenderer(renderer: TimerRenderer): void {
+	timerState = TimerState.RUN
+
+	private togglePaused(): void {
+		if (this.timerState == TimerState.RUN) {
+			this.timerState = TimerState.PAUSE_STARTED_SHOW_MSG
+		} else if (this.timerState == TimerState.PAUSED) {
+			this.timerState = TimerState.RUN
+		}
+	}
+
+	private endTimer(): void {
+		console.log('')
+		clearInterval(this.nodeTimer)
+		process.exit(0)
+	}
+
+	private renderTimerToConsole(renderer: TimerRenderer): void {
+		// If we're already paused with a pause message, just return
+		if (this.timerState == TimerState.PAUSED) {
+			return
+		}
+
 		this.iterations++
 		const now = new Date()
 		const details: TimerDetails = this.details(now)
 		const matrix: StringMatrix = renderer.render(details)
 
+		// If pause has been started, render a PAUSED message on top of the rendering
+		if (this.timerState == TimerState.PAUSE_STARTED_SHOW_MSG) {
+			Timer.addPausedMessage(matrix)
+			this.timerState = TimerState.PAUSED
+		}
+
 		AnsiCursor.renderTopLeft(matrix.toString())
 
 		if (now > this.endingAt) {
-			console.log('')
-			clearInterval(this.nodeTimer)
+			this.endTimer()
 		}
+	}
+
+	private static addPausedMessage(matrix: StringMatrix): void {
+		const message = '   ' + '       \n  PAUSED  \n          '
+		const overlay = StringMatrix.fromMultilineMonochromeString(message)
+		overlay.addDoubleLineBox(new Rectangle(0, 0, 9, 2), Xterm256.RED_1)
+		matrix.overlayCentered(overlay, '\u2500')
 	}
 
 	private details(now: Date): TimerDetails {
@@ -61,6 +103,7 @@ export default class Timer extends Command {
 			0,
 			Math.floor((this.endingAt.getTime() - new Date().getTime()) / 1000)
 		)
+		// const elapsedSeconds = Math.floor((now.getTime() - this.createdAt.getTime()) / 1000)
 
 		return new TimerDetails(
 			this.createdAt,
@@ -76,9 +119,14 @@ export default class Timer extends Command {
 		if (this.renderer === null) {
 			Timer.exitWithError('Error: No renderer found')
 		} else {
-			this.executeRenderer(this.renderer)
+			this.renderTimerToConsole(this.renderer)
 		}
 	}
+
+	// private setDuration(seconds: number): void {
+	// 	this.durationSeconds = seconds
+	// 	this.endingAt = new Date(this.createdAt.getTime() + seconds * 1000)
+	// }
 
 	async run(): Promise<void> {
 		const { args } = await this.parse(Timer)
@@ -91,6 +139,17 @@ export default class Timer extends Command {
 		// Hide the cursor now and restore it when the program exits
 		AnsiCursor.hideCursor()
 		onExit(() => AnsiCursor.showCursor())
+
+		readline.emitKeypressEvents(process.stdin)
+		process.stdin.setRawMode(true)
+		process.stdin.on('keypress', (str, key) => {
+			if (key.ctrl && key.name === 'c') {
+				process.exit() // eslint-disable-line no-process-exit
+			}
+			if (str === ' ') {
+				this.togglePaused()
+			}
+		})
 
 		this.timerCallback()
 
