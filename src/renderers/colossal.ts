d85e92ccdaeb8913201d8b2fb3fdea7dbdb872f0
Jason Swartz <dev@bks2.com>
2022-09-18 14:57:09 -0700

    Looks great now!

--- src/renderers/colossal.ts
+++ src/renderers/colossal.ts
@@ -4,51 +4,36 @@ import { TimerDetails, TimerRenderer } from './timer-renderer'
 import { FigletFonts, Fonts } from '../common/fonts'
 import { TextBlocks } from '../common/textblocks'
 import { XtermColorGradients } from '../common/xtermcolorgradients'
-import { StringUtils } from '../common/stringutils'
 import { Rectangle } from '../common/Rectangle'
 import { RenderUtils } from './renderutils'
 
 class Colossal implements TimerRenderer {
 	readonly doubleColorGradient = XtermColorGradients.doubleColorGradientOrExit(Xterm256.BLUE_1, Xterm256.BLUEVIOLET)
 	readonly timeRemainingGradient = this.doubleColorGradient.concat(this.doubleColorGradient.reverse())
-	readonly MAX_COLOSSAL_WIDTH = 60 // 48
+	readonly MAX_COLOSSAL_WIDTH = 50
 
 	/**
 	 * This renderer displays a large countdown timer in the Colossal Figlet font
 	 * @param details information about the current timer in-progress
 	 */
 	render(details: TimerDetails): StringMatrix {
+		const horizPadding = 1
+		const vertPadding = 1
+
 		const gradientStart = details.iteration % this.timeRemainingGradient.length
 		const gradient = this.timeRemainingGradient
 			.slice(gradientStart)
 			.concat(this.timeRemainingGradient.slice(0, gradientStart))
 
-		let timeRemaining = Colossal.renderTimeRemainingFiglet(details)
-		const timeRemainingLines = timeRemaining.split(StringUtils.NEWLINE)
-		const rows = timeRemainingLines.length
-		const maxRowWidth = TextBlocks.maxRowWidth(timeRemaining)
-
-		const horizPadding = 4
-		const vertPadding = 2
-
-		const padding = new Rectangle(
-			horizPadding,
-			vertPadding,
-			this.MAX_COLOSSAL_WIDTH - maxRowWidth + horizPadding,
-			vertPadding
-		)
-		timeRemaining = TextBlocks.setAllPadding(timeRemaining, padding, ' ')
+		const timeRemaining = Colossal.renderTimeRemainingFiglet(details)
 
 		const timeRemainingMatrix = StringMatrix.fromMultilineMonochromeString(timeRemaining)
+		timeRemainingMatrix.padLeft(horizPadding)
+		timeRemainingMatrix.padRight(this.MAX_COLOSSAL_WIDTH - timeRemainingMatrix.cols() + horizPadding * 2)
+		timeRemainingMatrix.addVertPadding(vertPadding, vertPadding)
 		timeRemainingMatrix.setVerticalGradient(gradient)
 
-		const boxOffset = 1
-		const boxBounds = new Rectangle(
-			horizPadding - boxOffset - 1,
-			vertPadding - boxOffset - 1,
-			this.MAX_COLOSSAL_WIDTH - horizPadding - boxOffset,
-			rows + vertPadding - boxOffset
-		)
+		const boxBounds = new Rectangle(0, 0, timeRemainingMatrix.cols() - 1, timeRemainingMatrix.rows() - 1)
 		const boxColor = RenderUtils.getGreenYellowRedColor(details.percentDone())
 		timeRemainingMatrix.addDoubleLineBox(boxBounds, boxColor)
 
