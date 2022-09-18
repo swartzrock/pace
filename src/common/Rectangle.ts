9058e56b10a785206f7a91274669f2b613a47385
Jason Swartz <dev@bks2.com>
2022-09-18 14:56:59 -0700

    added toString()

--- src/common/Rectangle.ts
+++ src/common/Rectangle.ts
@@ -1,5 +1,8 @@
 class Rectangle {
 	constructor(public left: number, public top: number, public right: number, public bottom: number) {}
+	toString(): string {
+		return `Rectangle(${this.left}, ${this.top}, ${this.right}, ${this.bottom})`
+	}
 }
 
 export { Rectangle }
