1b22517f529728a719ce527e71ae06f00e02ce3e
Jason Swartz <dev@bks2.com>
2022-09-25 13:01:38 -0700

    Now with a fancy time remaining shadow colored with the existing pie chart colors and a fancier time remaining gradient.

--- src/renderers/pie.ts
+++ src/renderers/pie.ts
@@ -5,26 +5,26 @@ import { TimerDetails, TimerRenderer } from './timer-renderer'
 import { FigletFonts, Fonts } from '../common/fonts'
 import { TextBlocks } from '../common/textblocks'
 import { RenderUtils } from './renderutils'
+import { Point } from '../common/point'
+import { Utils } from '../common/utils'
+import { UnicodeChars } from '../common/unicodechars'
+import { XtermColorGradients } from '../common/xtermcolorgradients'
 
 class PieChart implements TimerRenderer {
-	readonly CHART_FILL_CHAR = '\u2588'
+	readonly CHART_FILL_CHAR = UnicodeChars.BLOCK_FULL
+	readonly CHART_SHADOW_CHAR = UnicodeChars.SHADE_DARK
+
 	readonly CHART_EMPTY_CHAR = 'z'
 	readonly CHART_FILL_COLORS = [Xterm256.GREEN_1, Xterm256.GREENYELLOW, Xterm256.RED_1]
 	readonly CHART_EMPTY_COLOR = Xterm256.GREY_007
 
-	static MONOCHROME_GRADIENT: Xterm256[] = [
-		Xterm256.GREY_058,
-		Xterm256.GREY_062,
-		Xterm256.GREY_066,
-		Xterm256.GREY_070,
-		Xterm256.GREY_074,
-		Xterm256.GREY_078,
-		Xterm256.GREY_082,
-		Xterm256.GREY_085,
-		Xterm256.GREY_089,
-		Xterm256.GREY_093,
-		Xterm256.GREY_100,
-	]
+	static TIME_REMAINING_GRADIENT: Xterm256[] = XtermColorGradients.doubleColorGradientOrExit(
+		Xterm256.PURPLEA,
+		Xterm256.PURPLEB
+	)
+		.slice(0, 7)
+		.reverse()
+
 	pieChart = new SquarePieChart()
 
 	/**
@@ -39,15 +39,23 @@ class PieChart implements TimerRenderer {
 		const coloredFillChar = Colors.foregroundColor(this.CHART_FILL_CHAR, fillColor)
 		const coloredEmptyChar = Colors.foregroundColor(this.CHART_FILL_CHAR, this.CHART_EMPTY_COLOR)
 
-		// centeredMonoChartMatrix.setVerticalGradientDithered(this.timeRemainingGradient, this.CHART_FILL_CHAR)
 		centeredMonoChartMatrix.replaceAll(this.CHART_FILL_CHAR, coloredFillChar)
 		centeredMonoChartMatrix.replaceAll(this.CHART_EMPTY_CHAR, coloredEmptyChar)
 
 		const timeRemaining = PieChart.renderTimeRemainingFiglet(details)
-		const timeRemainingMatrix = StringMatrix.createFromMultilineMonoString(timeRemaining)
 
-		timeRemainingMatrix.setVerticalGradient(PieChart.MONOCHROME_GRADIENT)
-		centeredMonoChartMatrix.overlayCentered(timeRemainingMatrix, ' ')
+		const timeRemainingMatrixShadow = StringMatrix.createFromMultilineMonoString(timeRemaining)
+		const shadowLoc = new Point(
+			Utils.halfInt(centeredMonoChartMatrix.cols()) - Utils.halfInt(timeRemainingMatrixShadow.cols()) + 1,
+			Utils.halfInt(centeredMonoChartMatrix.rows()) - Utils.halfInt(timeRemainingMatrixShadow.rows()) + 1
+		)
+
+		timeRemainingMatrixShadow.replaceAll('█', this.CHART_SHADOW_CHAR)
+		centeredMonoChartMatrix.overlayAt(timeRemainingMatrixShadow, shadowLoc, undefined, true)
+
+		const timeRemainingMatrix = StringMatrix.createFromMultilineMonoString(timeRemaining)
+		timeRemainingMatrix.setVerticalGradient(PieChart.TIME_REMAINING_GRADIENT)
+		centeredMonoChartMatrix.overlayCentered(timeRemainingMatrix)
 
 		return centeredMonoChartMatrix
 	}
