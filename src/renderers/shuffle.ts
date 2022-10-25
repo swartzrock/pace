78c79f0194846ae59d1e9bdd6a5790cd140c83cf
Jason Swartz <dev@bks2.com>
2022-10-24 22:31:07 -0700

    Displaying the remaining time and upcoming renderer in the status bar now

--- src/renderers/shuffle.ts
+++ src/renderers/shuffle.ts
@@ -2,7 +2,11 @@ import {StringMatrix} from '../common/stringmatrix'
 import {AllRenderers, RendererInfo, TimerRenderer} from './timerRenderer'
 import {TimerDetails} from './timerDetails'
 import {Point} from '../common/point'
+import {Timer} from "../commands/timer";
 
+/**
+ * todo show a vertical line fading from one renderer to the next
+ */
 class Shuffle implements TimerRenderer {
 	readonly ITERATIONS_PER_RENDERER = 75
 
@@ -14,8 +18,15 @@ class Shuffle implements TimerRenderer {
 	render(details: TimerDetails, terminalDims: Point): StringMatrix {
 		const renderers: Array<RendererInfo> = AllRenderers.renderers.filter((i) => i.name != 'shuffle')
 		const index = Math.floor(details.iteration / this.ITERATIONS_PER_RENDERER) % renderers.length
+
+		const INTERVALS_PER_SECOND = 1000 / Timer.TIMER_CALLBACK_INTERVAL_MS
+		const remainingSeconds = Math.floor((this.ITERATIONS_PER_RENDERER - details.iteration % this.ITERATIONS_PER_RENDERER) / INTERVALS_PER_SECOND)
 		const selected = renderers[index]
-		details.statusBarMessage = `Shuffle: displaying "${selected.name}"`
+		if (remainingSeconds > 1) {
+			details.statusBarMessage = `Shuffle: showing the "${selected.name}" renderer. ${remainingSeconds} seconds remaining.`
+		} else {
+			details.statusBarMessage = `Shuffle: loading the "${renderers[(index + 1) % renderers.length].name}" renderer...`
+		}
 		return selected.renderer.render(details, terminalDims)
 	}
 }
