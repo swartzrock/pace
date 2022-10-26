a181a177361cb3520348bf192e08df31b7cc4df4
Jason Swartz <dev@bks2.com>
2022-10-25 21:06:39 -0700

    Much better shuffle! Now cross-fading to the next renderer and shuffling (randomizing) the list of renderers.

--- src/renderers/shuffle.ts
+++ src/renderers/shuffle.ts
@@ -3,31 +3,58 @@ import {AllRenderers, RendererInfo, TimerRenderer} from './timerRenderer'
 import {TimerDetails} from './timerDetails'
 import {Point} from '../common/point'
 import {Timer} from "../commands/timer";
+import * as _ from 'lodash'
 
-/**
- * todo show a vertical line fading from one renderer to the next
- */
 class Shuffle implements TimerRenderer {
-	readonly ITERATIONS_PER_RENDERER = 75
+	readonly ITERATIONS_PER_RENDERER = 100
+	readonly SHOW_NEXT_RENDERER_PERCENT = 0.8
+
+	renderers: Array<RendererInfo> = []
 
 	/**
-	 * This renderer displays a large countdown timer in the Colossal Figlet font
+	 * Shuffle shuffles between the renderers, playing them for 10 seconds, plus a cross-fade effect.
 	 * @param details information about the current timer in-progress
 	 * @param terminalDims the current terminal dimensions
 	 */
 	render(details: TimerDetails, terminalDims: Point): StringMatrix {
-		const renderers: Array<RendererInfo> = AllRenderers.renderers.filter((i) => i.name != 'shuffle')
-		const index = Math.floor(details.iteration / this.ITERATIONS_PER_RENDERER) % renderers.length
+
+		if (this.renderers.length == 0) {
+			this.renderers = _.shuffle(AllRenderers.renderers.filter((i) => i.name != 'shuffle'))
+		}
+
+		const currentIndex = Math.floor(details.iteration / this.ITERATIONS_PER_RENDERER) % this.renderers.length
+		const nextIndex = (currentIndex + 1) % this.renderers.length
+
+		const currentMatrix = this.renderers[currentIndex].renderer.render(details, terminalDims)
+		currentMatrix.fitToWindow()
+
+		const pctDone = (details.iteration % this.ITERATIONS_PER_RENDERER) / this.ITERATIONS_PER_RENDERER
+		if (pctDone > this.SHOW_NEXT_RENDERER_PERCENT) {
+
+			const nextPct = 1.0 - (1.0 - pctDone) / (1.0 - this.SHOW_NEXT_RENDERER_PERCENT)
+			const nextColumns = nextPct * currentMatrix.cols()
+
+			const nextMatrix = this.renderers[nextIndex].renderer.render(details, terminalDims)
+			nextMatrix.fitToWindow()
+
+			const cols = Math.min(nextColumns, nextMatrix.cols())
+			const rows = Math.min(currentMatrix.rows(), nextMatrix.rows())
+			for (let row = 0; row < rows; row++) {
+				for (let col = 0; col < cols; col++) {
+					currentMatrix.setCell(nextMatrix.getCell(col, row), col, row)
+				}
+			}
+		}
 
 		const INTERVALS_PER_SECOND = 1000 / Timer.TIMER_CALLBACK_INTERVAL_MS
 		const remainingSeconds = Math.floor((this.ITERATIONS_PER_RENDERER - details.iteration % this.ITERATIONS_PER_RENDERER) / INTERVALS_PER_SECOND)
-		const selected = renderers[index]
 		if (remainingSeconds > 1) {
-			details.statusBarMessage = `Shuffle: showing the "${selected.name}" renderer. ${remainingSeconds} seconds remaining.`
+			details.statusBarMessage = `Shuffle: showing the "${this.renderers[currentIndex].name}" renderer for ${remainingSeconds} more seconds...`
 		} else {
-			details.statusBarMessage = `Shuffle: loading the "${renderers[(index + 1) % renderers.length].name}" renderer...`
+			details.statusBarMessage = `Shuffle: loading the "${this.renderers[nextIndex].name}" renderer...`
 		}
-		return selected.renderer.render(details, terminalDims)
+
+		return currentMatrix
 	}
 }
 
