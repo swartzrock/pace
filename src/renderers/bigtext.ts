0231b1d180ae36cc7582bba203d7041934141cb2
Jason Swartz <dev@bks2.com>
2022-10-26 23:19:11 -0700

    Removed unused code, calculating how many times to double the matrix based on a theoretical max width of "55:55" which prevents unintended resizing as the countdown numbers change.

--- src/renderers/bigtext.ts
+++ src/renderers/bigtext.ts
@@ -1,40 +1,65 @@
-import { TimerRenderer } from './timerRenderer'
-import { Colors, Xterm256 } from '../common/colors'
-import { TimerDetails } from './timerDetails'
-import { StringMatrix } from '../common/stringmatrix'
-import { UnicodeChars } from '../common/unicodechars'
-import { SquarePieChart } from '../common/squarepiechart'
-import { TextBlocks } from '../common/textblocks'
-import { Utils } from '../common/utils'
-import { Point } from '../common/point'
-import { FigletFonts, Fonts } from '../common/fonts'
-import { XtermGradients } from '../common/xtermgradients'
+import {TimerRenderer} from './timerRenderer'
+import {TimerDetails} from './timerDetails'
+import {StringMatrix} from '../common/stringmatrix'
+import {TextBlocks} from '../common/textblocks'
+import {Utils} from '../common/utils'
+import {Point} from '../common/point'
+import {FigletFonts, Fonts} from '../common/fonts'
+import {XtermGradients} from '../common/xtermgradients'
+import * as _ from 'lodash'
 
 class BigText implements TimerRenderer {
-	readonly REMAINING_CHAR = Colors.foregroundColor('.', Xterm256.GREY_007)
-	readonly HAND_CHAR = Colors.foregroundColor(UnicodeChars.BLOCK_FULL, Xterm256.PURPLE_4A)
-	readonly SLICE_PERCENT = 0.01
-	readonly RADIUS_MARGIN = 4
 
-	pieChart = new SquarePieChart()
+	readonly FONT = FigletFonts.ANSI_REGULAR
+
+	cachedDoubler: number | undefined
+	cachedTerminalDims: Point | undefined
 
 	render(details: TimerDetails, terminalDims: Point): StringMatrix {
-		const font = FigletFonts.ANSI_REGULAR
 
-		const maxCols = terminalDims.col - 4
-		const maxRows = terminalDims.row - 4
-
-		const timeRemainingFiglet = TextBlocks.trimToContent(Fonts.render(font, details.timeRemainingText()))
+		const timeRemainingFiglet = TextBlocks.trimToContent(Fonts.render(this.FONT, details.timeRemainingText()))
 		const timeRemainingMatrix = StringMatrix.createFromMultilineMonoString(timeRemainingFiglet)
 
-		while (timeRemainingMatrix.cols() * 2 < maxCols && timeRemainingMatrix.rows() * 2 < maxRows) {
+		for (let i = 0; i < this.getDoubler(terminalDims); i++) {
 			timeRemainingMatrix.matrix = timeRemainingMatrix.matrix.map((row) => Utils.double(row))
 			timeRemainingMatrix.matrix = Utils.double(timeRemainingMatrix.matrix)
 		}
+
 		timeRemainingMatrix.setVerticalGradient(XtermGradients.SINGLE_COLOR_GRADIENTS.BLUE_1_TO_CYAN_1)
 
 		return timeRemainingMatrix
 	}
+
+	/**
+	 * Return the number of times the matrix can be doubled and fit in the terminal
+	 * @param terminalDims
+	 */
+	getDoubler(terminalDims: Point): number {
+
+		if (this.cachedDoubler && this.cachedTerminalDims && _.isEqual(this.cachedTerminalDims, terminalDims)) {
+			return this.cachedDoubler
+		}
+
+		const maxCols = terminalDims.col - 4
+		const maxRows = terminalDims.row - 4
+
+		const timeRemainingFiglet = TextBlocks.trimToContent(Fonts.render(this.FONT, "55:55"))
+		const timeRemainingMatrix = StringMatrix.createFromMultilineMonoString(timeRemainingFiglet)
+
+		let doubler = 0
+		while (timeRemainingMatrix.cols() * 2 < maxCols && timeRemainingMatrix.rows() * 2 < maxRows) {
+			timeRemainingMatrix.matrix = timeRemainingMatrix.matrix.map((row) => Utils.double(row))
+			timeRemainingMatrix.matrix = Utils.double(timeRemainingMatrix.matrix)
+			doubler = doubler + 1
+		}
+
+		this.cachedDoubler = doubler
+		this.cachedTerminalDims = terminalDims
+
+		return doubler
+	}
+
+
 }
 
 export { BigText }
