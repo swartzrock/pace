663f8daf8dc6e01e39ea1ac6158e0ba8e6e575e7
Jason Swartz <dev@bks2.com>
2022-10-30 21:58:40 -0700

    Now showing the time remaining, with a gradient and shadow

--- src/renderers/sine.ts
+++ src/renderers/sine.ts
@@ -1,4 +1,4 @@
-import {Colors} from '../common/colors'
+import {Colors, Xterm256} from '../common/colors'
 import {StringMatrix} from '../common/stringmatrix'
 import {TimerRenderer} from './timerRenderer'
 import {TimerDetails} from './timerDetails'
@@ -6,14 +6,19 @@ import {Point} from '../common/point'
 import {UnicodeChars} from "../common/unicodechars";
 import {Utils} from "../common/utils";
 import {XtermGradients} from "../common/xtermgradients";
+import {FigletFonts, Fonts} from "../common/fonts";
+import {TextBlocks} from "../common/textblocks";
 
 class Sine implements TimerRenderer {
 
-	private readonly WAVE_LENGTH_COLS = 40;
+	private readonly WAVE_LENGTH_COLS = 40
+	private readonly MIN_COLUMN = 5 // the sine wave looks funny with 1 column, so start with 5
 
 	readonly GREEN_GRADIENT = XtermGradients.SINGLE_COLOR_GRADIENTS.BLUE_1_TO_CYAN_1
 	readonly YELLOW_GRADIENT = XtermGradients.SINGLE_COLOR_GRADIENTS.YELLOW_3B_TO_LIGHTSTEELBLUE_1
 	readonly RED_GRADIENT = XtermGradients.SINGLE_COLOR_GRADIENTS.RED_3B_TO_MAGENTA_2A
+	readonly TIME_REMAINING_GRADIENT: Xterm256[] = Utils.reverse(XtermGradients.SINGLE_COLOR_GRADIENTS.BLUE_1_TO_CYAN_1)
+	readonly SHADOW_CHAR = Colors.foregroundColor(UnicodeChars.FULL_CIRCLE, Xterm256.GREY_000)
 
 
 	/**
@@ -24,8 +29,8 @@ class Sine implements TimerRenderer {
 	render(details: TimerDetails, terminalDims: Point): StringMatrix {
 
 		const progressBarLength = terminalDims.col - 8
-		const barCompleteLen = Math.floor(details.percentDone() * progressBarLength)
-		const graphHeight = terminalDims.row * 2 / 3
+		const barCompleteLen = Math.max(this.MIN_COLUMN, Math.floor(details.percentDone() * progressBarLength))
+		const graphHeight = terminalDims.row / 2
 		const gradient = this.getGradient(details)
 
 		const matrix = StringMatrix.createUniformMatrix(terminalDims.col, graphHeight)
@@ -34,7 +39,25 @@ class Sine implements TimerRenderer {
 			this.plotSine(matrix, col, sineValue, gradient)
 		}
 
-		return matrix // StringMatrix.createUniformMatrix(terminalDims.col, terminalDims.row, ' ')
+		const timeRemainingFiglet = Fonts.render(FigletFonts.ANSI_REGULAR, details.timeRemainingText())
+		const timeRemaining = TextBlocks.setPadding(timeRemainingFiglet, 1, 1, ' ')
+
+		// render the time-remaining shadow
+		const timeRemainingMatrixShadow = StringMatrix.createFromMultilineMonoString(timeRemaining)
+		timeRemainingMatrixShadow.double()
+		const shadowOffset = new Point(1, 1)
+		timeRemainingMatrixShadow.replaceAll(UnicodeChars.BLOCK_FULL, this.SHADOW_CHAR)
+		matrix.overlayCentered(timeRemainingMatrixShadow, undefined, true, shadowOffset)
+
+		// render the time-remaining text
+		const timeRemainingMatrix = StringMatrix.createFromMultilineMonoString(timeRemaining)
+		timeRemainingMatrix.double()
+		timeRemainingMatrix.replaceAll(UnicodeChars.BLOCK_FULL, UnicodeChars.HALF_CIRCLE)
+		timeRemainingMatrix.setVerticalGradient(this.TIME_REMAINING_GRADIENT)
+		matrix.overlayCentered(timeRemainingMatrix)
+
+
+		return matrix
 	}
 
 	plotSine(matrix: StringMatrix, col: number, sineValue: number, gradient: number[]) {
