06c1abc276363ee7f769b87ef487fea0a5b5b5c1
Jason Swartz <dev@bks2.com>
2022-11-05 22:51:01 -0700

    added plus()

--- src/common/point.ts
+++ src/common/point.ts
@@ -1,5 +1,9 @@
 class Point {
 	constructor(public col: number, public row: number) {}
+	plus(p: Point) {
+		this.col = this.col + p.col
+		this.row = this.row + p.row
+	}
 	toString(): string {
 		return `Point(${this.col}, ${this.row})`
 	}
