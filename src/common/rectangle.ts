13a76e7f25ee01bec8969a463b5147341bbf1411
Jason Swartz <dev@bks2.com>
2022-11-05 20:55:24 -0700

    added contains()

--- src/common/rectangle.ts
+++ src/common/rectangle.ts
@@ -1,5 +1,11 @@
+import {Utils} from "./utils";
+import {Point} from "./point";
+
 class Rectangle {
 	constructor(public left: number, public top: number, public right: number, public bottom: number) {}
+	contains(p: Point) {
+		return Utils.between(p.col, this.left, this.right) && Utils.between(p.row, this.top, this.bottom)
+	}
 	toString(): string {
 		return `Rectangle(${this.left}, ${this.top}, ${this.right}, ${this.bottom})`
 	}
