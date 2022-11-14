dc8aa68cf0599e67011800fe26dcc022eb256f27
Jason Swartz <dev@bks2.com>
2022-11-13 22:12:15 -0800

    normalized the height map, removed time remaining overlay with a double line box and time remaining at the top, replaced blocks with quarter circle, looks great!

--- src/renderers/lava.ts
+++ src/renderers/lava.ts
@@ -1,4 +1,4 @@
-import {Colors, Xterm256} from '../common/colors'
+import {Colors} from '../common/colors'
 import {StringMatrix} from '../common/stringmatrix'
 import {TimerRenderer} from './timerRenderer'
 import {TimerDetails} from './timerDetails'
@@ -6,7 +6,7 @@ import {Point} from '../common/point'
 import {UnicodeChars} from "../common/unicodechars";
 import {Utils} from "../common/utils";
 import {XtermGradients} from "../common/xtermgradients";
-import {TextEffects} from "../common/textEffects";
+import {Rectangle} from "../common/rectangle";
 
 /**
  * A lava-lamp (or "plasma") renderer.
@@ -14,18 +14,20 @@ import {TextEffects} from "../common/textEffects";
  */
 class Lava implements TimerRenderer {
 
-	private readonly SHADOW_CHAR = Colors.foregroundColor(UnicodeChars.FULL_CIRCLE, Xterm256.GREY_000)
 	private readonly MAP_SIZE = 800
 	private readonly MAP_STRETCH = (3 * Math.PI) / (this.MAP_SIZE / 2)
-
 	private readonly MATRIX_TERMINAL_PCT = new Point(0.75, 0.6)
 	private readonly LAVA_GRADIENT_ITERATIONS = 200
+	private readonly LAVA_GRADIENT_MULTIPLE = 7
+	private readonly LAVA_CHAR = UnicodeChars.QUARTER_CIRCLE
 
-	private lavaGradient: Array<number> = [] // Utils.concatReversed(XtermGradients.DOUBLE_COLOR_GRADIENTS.BLUEVIOLET_TO_BLUE_1)
+	private lavaGradient: Array<number> = []
 
+	// Height map from 0 to 1
 	private heightMap1: Array<number> = []
 	private heightMap1Origin = new Point(0, 0)
 
+	// Height map from 0 to 1
 	private heightMap2: Array<number> = []
 	private heightMap2Origin = new Point(0, 0)
 
@@ -42,14 +44,20 @@ class Lava implements TimerRenderer {
 			for (let col = 0; col < matrix.cols(); col++) {
 				const heightMap1Index = (row + this.heightMap1Origin.row) * this.MAP_SIZE + (col + this.heightMap1Origin.col)
 				const heightMap2Index = (row + this.heightMap2Origin.row) * this.MAP_SIZE + (col + this.heightMap2Origin.col)
-				const fgColor = Utils.getModulusNonEmpty(this.lavaGradient, this.heightMap1[heightMap1Index] + this.heightMap2[heightMap2Index])
-				const fg = Colors.foregroundColor(UnicodeChars.BLOCK_FULL, fgColor)
+				const combinedHeight = this.heightMap1[heightMap1Index] + this.heightMap2[heightMap2Index]
+				const gradientIndex = Math.floor(combinedHeight * this.lavaGradient.length * this.LAVA_GRADIENT_MULTIPLE)
+				const fgColor = Utils.getModulusNonEmpty(this.lavaGradient, gradientIndex)
+				const fg = Colors.foregroundColor(this.LAVA_CHAR, fgColor)
 				matrix.setCell(fg, col, row)
 			}
 		}
 
-		const timeRemainingGradient = TimerRenderer.getGreenYellowRedGradient(details.percentDone())
-		TextEffects.renderShadowedText(details.timeRemainingText(), matrix, timeRemainingGradient, UnicodeChars.BLOCK_FULL, this.SHADOW_CHAR)
+		matrix.pad(new Rectangle(2, 2, 2, 2))
+		const boxRect = new Rectangle(0,0,matrix.cols() - 1,matrix.rows() - 1		)
+		matrix.addDoubleLineBox(boxRect, TimerRenderer.getGreenYellowRedColor(details.percentDone()))
+		const timeRemainingText = ` ${details.timeRemainingText()} remaining `
+		matrix.setHorizontallyCenteredMonochromeString(timeRemainingText, 0)
+
 		return matrix
 	}
 
@@ -70,13 +78,14 @@ class Lava implements TimerRenderer {
 	 * Select new sampling points for the height maps using the current epoch time
 	 */
 	moveHeightMapOrigins() {
-		const time = Date.now()
+		const seedX = Date.now() * 0.0002
+		const seedY = Date.now() * 0.0003
 
-		this.heightMap1Origin.col = Math.floor((((Math.cos(time * 0.0002 + 0.4 + Math.PI) + 1) / 2) * this.MAP_SIZE) / 2)
-		this.heightMap1Origin.row = Math.floor((((Math.cos(time * 0.0003 - 0.1) + 1) / 2) * this.MAP_SIZE) / 2)
+		this.heightMap1Origin.col = Math.floor((((Math.cos(seedX + 0.4 + Math.PI) + 1) / 2) * this.MAP_SIZE) / 2)
+		this.heightMap1Origin.row = Math.floor((((Math.cos(seedY - 0.1) + 1) / 2) * this.MAP_SIZE) / 2)
 
-		this.heightMap2Origin.col = Math.floor((((Math.cos(time * -0.0002 + 1.2) + 1) / 2) * this.MAP_SIZE) / 2)
-		this.heightMap2Origin.row = Math.floor((((Math.cos(time * -0.0003 - 0.8 + Math.PI) + 1) / 2) * this.MAP_SIZE) / 2)
+		this.heightMap2Origin.col = Math.floor((((Math.cos(0 - seedX + 1.2) + 1) / 2) * this.MAP_SIZE) / 2)
+		this.heightMap2Origin.row = Math.floor((((Math.cos(0 - seedY - 0.8 + Math.PI) + 1) / 2) * this.MAP_SIZE) / 2)
 	}
 
 	/**
@@ -91,9 +100,7 @@ class Lava implements TimerRenderer {
 		for (let row = 0; row < this.MAP_SIZE; row++) {
 			for (let col = 0; col < this.MAP_SIZE; col++) {
 				const centeredPoint = new Point(col - this.MAP_SIZE / 2, row - this.MAP_SIZE / 2 )
-				const waveHeight = Math.sin(distanceFromOrigin(centeredPoint) * this.MAP_STRETCH)
-				const normalizedWaveHeight = (waveHeight + 1) / 2
-				this.heightMap1[row * this.MAP_SIZE + col] = Math.floor(normalizedWaveHeight * 128)
+				this.heightMap1[row * this.MAP_SIZE + col] = (Math.sin(distanceFromOrigin(centeredPoint) * this.MAP_STRETCH) + 1) / 2
 			}
 		}
 
@@ -102,12 +109,9 @@ class Lava implements TimerRenderer {
 				const centeredPoint = new Point(col - this.MAP_SIZE / 2, row - this.MAP_SIZE / 2 )
 				const skewedDistance1 = distanceFromOrigin(new Point(centeredPoint.col * 0.8, centeredPoint.row * 1.3)) * 0.022
 				const skewedDistance2 = distanceFromOrigin(new Point(centeredPoint.col * 1.35, centeredPoint.row * 0.45)) * 0.022
-				const waveHeight = Math.sin(skewedDistance1) + Math.cos(skewedDistance2)
-				const normalizedWaveHeight = (waveHeight + 2) / 4
-				this.heightMap2[row * this.MAP_SIZE + col] = Math.floor(normalizedWaveHeight * this.MAP_SIZE / 4)
+				this.heightMap2[row * this.MAP_SIZE + col] = (Math.sin(skewedDistance1) + Math.cos(skewedDistance2) + 2) / 4
 			}
 		}
-
 
 	}
 
